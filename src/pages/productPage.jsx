6c8b2495f9de5e4b5d79104672d2551ca386cac5
YasiruC <info.yasiruc@gmail.com>
2026-05-18 03:56:55 +0530

    add search functionality to product page 🚀

--- src/pages/productPage.jsx
+++ src/pages/productPage.jsx
@@ -2,10 +2,12 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 import toast from "react-hot-toast";
 import ProductCard from "../components/productCard";
+import api from "../utils/api";
 
 export default function ProductPage(){
 
     const [products, setProducts] = useState([]);
+    const [searchQuery, setSearchQuery] = useState("");
     const [isLoading, setIsLoading] = useState(false);
 
     useEffect(
@@ -26,9 +28,24 @@ export default function ProductPage(){
         ,[isLoading]
     )
 
+    async function searchProducts(){
+        try{
+            const response = await api.get("/products/search/" + searchQuery);
+            setProducts(response.data);
+        }catch(error){
+            console.error("Error searching products:", error);
+            toast.error("Failed to search products");
+        }
+    }
+
 
     return(
-        <div className="w-full h-full flex justify-center flex-wrap">
+        <div className="w-full h-full flex justify-center flex-wrap relative pt-20">
+            <div className="w-full absolute top-0 left-0 h-[100px] flex items-center justify-center gap-4">
+                <input value={searchQuery} onChange={(e)=>{setSearchQuery(e.target.value)}} type="text" placeholder="Search products..." className="w-1/2 p-3 rounded-lg border border-gray-400 focus:outline-none focus:border-none focus:ring-2 focus:ring-specialColor"/>
+                <button className="bg-specialColor text-white p-3 rounded-lg hover:bg-hoverColor" onClick={searchProducts}>Search</button>
+                <button className="bg-specialColor text-white p-3 rounded-lg hover:bg-hoverColor" onClick={()=>{setIsLoading(false); setSearchQuery("");}}>All Product</button>
+            </div>
             {
                 products.map(
                     (product, index)=>{
